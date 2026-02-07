cd59bd40c68efca3340791feaa59e65bb9000270
Repon Hasan <reponh228@gmail.com>
2026-02-07 12:34:50 +0600

    SetRegisterForm

--- src/app/(commonlayout)/register/page.tsx
+++ src/app/(commonlayout)/register/page.tsx
@@ -1,10 +1,14 @@
 import RegistrationFrom from '@/components/RegistrationFrom'
 import React from 'react'
-
+import { Suspense } from "react";
+export const dynamic = "force-dynamic";
 function page() {
   return (
     <div className='h-[80vh] flex justify-center items-center'>
-      <RegistrationFrom></RegistrationFrom>
+        <Suspense fallback={null}>
+    <RegistrationFrom></RegistrationFrom>
+    </Suspense>
+      
     </div>
   )
 }
