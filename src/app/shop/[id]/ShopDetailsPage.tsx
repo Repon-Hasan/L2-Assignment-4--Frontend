2f0961024cb28a044770a0998d8e0daf90910686
Repon Hasan <reponh228@gmail.com>
2026-02-07 07:31:13 +0600

    FixedLink

--- src/app/shop/[id]/ShopDetailsPage.tsx
+++ src/app/shop/[id]/ShopDetailsPage.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Swal from "sweetalert2";
 import { format } from "date-fns";
+import Link from "next/link";
 
 interface Medicine {
   id: string;
